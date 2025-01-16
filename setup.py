e5f098d0b9fe262c7b773fb40c9c1058990e8189
Chris Daly <chris.daly@jisc.ac.uk>
2025-01-16 12:42:07 +0000

    UKDSB-687 - Updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='''ckanext-cookie-banner''',
+    name='''ckanext-cookie_banner''',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
 
     # The project's main homepage.
-    url='https://github.com//ckanext-cookie-banner',
+    url='https://github.com/ckanext-cookie-banner',
 
     # Author details
     author='''UKDS Jisc''',
@@ -84,7 +84,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        cookie-banner=ckanext.cookie_banner.plugin:UKDSCookieBannerPlugin
+        cookie_banner=ckanext.cookie_banner.plugin:UKDSCookieBannerPlugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
